6c4978f3fb1ddf6932cdbaa81887a7a8eadcacfa
Zoltanbi <zoltanbiro001@gmail.com>
2021-08-28 09:19:34 -0700

    setup logging service to avoid locking I/O & slowing application

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import config from 'config';
+import log from './logger'
 
 const port = config.get('port') as number;
 const host = config.get('host') as string;
@@ -10,5 +11,5 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, host, () => {
-    console.log(`Server listening at http://${host}:${port}`);
+    log.info(`Server listening at http://${host}:${port}`);
 });
